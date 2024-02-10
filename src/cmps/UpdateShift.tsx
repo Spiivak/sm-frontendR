06de309afbfa5ae36d25fe851bf20565e8cd4ab7
Spiivak <edenspiivak@gmail.com>
2024-02-10 17:58:48 +0200

    Add basic cmp

---
+++ src/cmps/UpdateShift.tsx
@@ -0,0 +1,9 @@
+import AppHeader from './AppHeader'
+
+export default function UpdateShift() {
+  return (
+    <section className='update-shift-index main-layout'>
+      <AppHeader/>
+    </section>
+  )
+}
