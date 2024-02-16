2c2466523b1f4de656e630cf1a4bd99c2510568b
Spiivak <edenspiivak@gmail.com>
2024-02-16 21:59:07 +0200

    Shift list now will know how to preview each type of shift

--- src/cmps/ShiftPreview.tsx
+++ src/cmps/ShiftPreview.tsx
@@ -10,7 +10,8 @@ interface ProductPreviewProps {
 export function ShiftPreview({ shift }: ProductPreviewProps) {
 
 
-  const { day: fromDay, month: fromMonth, dayName: toDayName } = shiftService.formatDate(shift.date.from)
+  const { day: fromDay, month: fromMonth, dayName: fromDayName } = shiftService.formatDate(shift.date.from)
+  const { day: toDay, month: toMonth, dayName: toDayName } = shiftService.formatDate(shift.date.to)
 
 
   const isToday = shiftService.isDateToday(new Date(shift.date.from))
@@ -23,20 +24,32 @@ export function ShiftPreview({ shift }: ProductPreviewProps) {
 
   const totalHours = shiftService.calculateTotalHours(shift)
   const totalEarned = shiftService.calculateTotalEarned(shift)
-  
+
   return (
     <div className={`shift-preview flex align-center ${isToday ? 'highlight' : ''}`}>
-      <div className="date-circle flex column align-center justify-center">
-        <p>{fromDay}/{fromMonth}</p>
-        <span>{toDayName}</span>
-      </div>
-      <div className="shift-information flex column">
-        <div className="time flex column">
-          <span className='flex gap8'>
-            <span className='from'>From - {fromHours}:{fromMinutes}</span>
-            <span className='to'>To - {toHours}:{toMinutes}</span>
-          </span>
+      {shift.type === 'normal' &&
+        <div className="date-circle flex column align-center justify-center">
+          <p>{fromDay}/{fromMonth}</p>
+          <span>{toDayName}</span>
         </div>
+      }
+      <div className="shift-information flex column">
+        {shift.type === 'normal' &&
+          <div className="time flex column">
+            <span className='flex gap8'>
+              <span className='from'>From - {fromHours}:{fromMinutes}</span>
+              <span className='to'>To - {toHours}:{toMinutes}</span>
+            </span>
+          </div>
+        }
+        {shift.type === 'dayoff' && <div>
+          <span>Day off</span>
+          <span>From {fromDay}/{fromMonth} To {toDay}/{toMonth}</span>
+        </div>}
+        {shift.type === 'sickday' && <div>
+          <span>Sick day</span>
+          <span>From {fromDay}/{fromMonth} To {toDay}/{toMonth}</span>
+        </div>}
         <div className="more-info flex gap16">
           <span className="total-hours">{totalHours} hours</span>
           <span className="total-earned">₪{totalEarned?.toFixed(2)}</span>
