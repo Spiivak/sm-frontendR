9f867f16f78f9a2fa1e0e2eef10ee9ab384c332f
Spiivak <edenspiivak@gmail.com>
2024-02-17 17:26:29 +0200

    Update dayyof and sickday classes Add edit shift on click shift Refactor full shiftpreview cmp

--- src/cmps/ShiftList.tsx
+++ src/cmps/ShiftList.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import { Shift } from "../services/shift.service"
 import { ShiftPreview } from "./ShiftPreview"
 
@@ -8,17 +9,22 @@ interface ShiftListProps {
 }
 
 export function ShiftList({ shifts, onRemoveShift, onEditShift }: ShiftListProps) {
-
+  const navigate = useNavigate()
+  const handleShiftEdit = (shift: Shift) => {
+    navigate(`${shift._id}/edit`)
+  }
 
   return (
     <section className='shifts-list'>
       {shifts.map((shift: Shift) => (
+        <div className="item" key={shift._id} onClick={() => handleShiftEdit(shift)}>
         <ShiftPreview
           key={shift._id}
           shift={shift}
           onRemoveShift={onRemoveShift}
           onEditShift={onEditShift}
-        />
+          />
+          </div>
       ))}
     </section>
   )
--- src/cmps/ShiftPreview.tsx
+++ src/cmps/ShiftPreview.tsx
@@ -9,61 +9,67 @@ interface ProductPreviewProps {
 }
 
 export function ShiftPreview({ shift }: ProductPreviewProps) {
-
-
-  const { day: fromDay, month: fromMonth, dayName: fromDayName } = shiftService.formatDate(shift.date.from)
+  const { day: fromDay, month: fromMonth } = shiftService.formatDate(shift.date.from)
   const { day: toDay, month: toMonth, dayName: toDayName } = shiftService.formatDate(shift.date.to)
-
-
   const isToday = shiftService.isDateToday(new Date(shift.date.from))
-
-
   const { fromHours, fromMinutes, toHours, toMinutes } = shift.time
     ? shiftService.extractTimeComponents([shift.time])
-    : { fromHours: [], fromMinutes: [], toHours: [], toMinutes: [] };
-
-
+    : { fromHours: [], fromMinutes: [], toHours: [], toMinutes: [] }
   const totalHours = shiftService.calculateTotalHours(shift)
   const totalEarned = shiftService.calculateTotalEarned(shift)
 
-  return (
-    <div className={`shift-preview flex align-center ${isToday ? 'highlight' : ''}`}>
-      {shift.type === 'normal' &&
-        <div className="date-circle flex column align-center justify-center">
-          <p>{fromDay}/{fromMonth}</p>
-          <span>{toDayName}</span>
-        </div>
-      }
-      {shift.type === 'dayoff' &&
-        <div className='type-icon'>
+  const renderShiftTypeIcon = () => {
+    switch (shift.type) {
+      case 'dayoff':
+        return <div className="type-icon">
           <VacationIcon />
         </div>
-      }
-
-      {shift.type === 'sickday' &&
-        <div className='type-icon'>
+      case 'sickday':
+        return (<div className='type-icon'>
           <SickIcon />
-        </div>
-      }
-      <div className="shift-information flex column">
-        {shift.type === 'normal' &&
+        </div>)
+      case 'normal':
+        return (
+          <div className="date-circle flex column align-center justify-center">
+            <p>{fromDay}/{fromMonth}</p>
+            <span>{toDayName}</span>
+          </div>
+
+        )
+      default:
+        return null
+    }
+  }
+
+  const renderShiftInfo = () => {
+    switch (shift.type) {
+      case 'normal':
+        return (
           <div className="time flex column">
             <span className='flex gap8'>
               <span className='from'>From - {fromHours}:{fromMinutes}</span>
               <span className='to'>To - {toHours}:{toMinutes}</span>
             </span>
           </div>
-        }
-        {shift.type === 'dayoff' &&
-          <div className='day-off flex gap8'>
-            <span>Day off</span>
+        )
+      case 'dayoff':
+      case 'sickday':
+        return (
+          <div className={`${shift.type}-info flex gap8`}>
+            <span>{shift.type === 'dayoff' ? 'Day off' : 'Sick day'}</span>
             <span>From {fromDay}/{fromMonth} To {toDay}/{toMonth}</span>
-          </div>}
-        {shift.type === 'sickday' &&
-          <div className='sick-day flex gap8'>
-            <span>Sick day</span>
-            <span>From {fromDay}/{fromMonth} To {toDay}/{toMonth}</span>
-          </div>}
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div className={`shift-preview flex align-center ${isToday ? 'highlight' : ''}`}>
+      <div className="type">{renderShiftTypeIcon()}</div>
+      <div className="shift-information flex column">
+        {renderShiftInfo()}
         <div className="more-info flex gap16">
           {shift.type === 'normal' && <span className="total-hours">{totalHours} hours</span>}
           <span className="total-earned">₪{totalEarned?.toFixed(2)}</span>
