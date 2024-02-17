d0d8a77036ff306d95464fd9af82d92fb0752063
Spiivak <edenspiivak@gmail.com>
2024-02-17 17:27:25 +0200

    Remove props Update active tab when editing shift

--- src/pages/UpdateShiftIndex.tsx
+++ src/pages/UpdateShiftIndex.tsx
@@ -6,8 +6,6 @@ import SickDay from '../cmps/UpdateShift/SickDay';
 import { Shift, shiftService } from '../services/shift.service';
 import { useParams } from 'react-router-dom';
 
-type SetActiveTabFunction = (tabName: string) => void;
-
 
 export default function UpdateShift() {
   const [activeTab, setActiveTab] = useState<string>('NewShift')
@@ -17,18 +15,28 @@ export default function UpdateShift() {
   useEffect(() => {
     loadShift()
 
-  }, [shiftId]);
+  }, [shiftId])
 
 
   const loadShift = async () => {
-    if (!shiftId) return;
+    if (!shiftId) return
     try {
-      const shiftData = await shiftService.getById(shiftId);
-      setShift(shiftData);
+      const shiftData = await shiftService.getById(shiftId)
+      setShift(shiftData)
+      
+      // Determine the initial active tab based on the shift type
+      if (shiftData.type === 'dayoff') {
+        setActiveTab('DayOff')
+      } else if (shiftData.type === 'sickday') {
+        setActiveTab('SickDay')
+      } else {
+        setActiveTab('NewShift')
+      }
     } catch (error) {
       console.error('Error loading shift:', error);
     }
-  }
+  };
+
   return (
     <section className='update-shift-index index'>
       <UpdateShiftHeader setActiveTab={setActiveTab} activeTab={activeTab}/>
