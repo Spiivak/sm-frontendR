e50afb8c954ff3c707d1aacb65170c63fed816cf
Spiivak <edenspiivak@gmail.com>
2024-02-22 18:47:38 +0200

    Impliment user login with firestore

--- src/pages/LoginIndex.tsx
+++ src/pages/LoginIndex.tsx
@@ -1,11 +1,43 @@
-import { signInWithPhoneNumber } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPhoneNumber } from 'firebase/auth'
+import { useState } from 'react'
+import { auth } from '../config/firebase.config'
+import { userService } from '../services/user.service'
+import { login } from '../store/actions/user.actions'
+import { useEffectUpdate } from '../customHooks/useEffectUpdate'
+import { useNavigate } from 'react-router-dom'
 
 
 type Props = {}
 
 const LoginIndex = (props: Props) => {
+  const navigate = useNavigate()
+
+  const [error, setError] = useState(false)
+  const [user, setUser] = useState<any>(userService.getLoggedinUser())
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState("")
+
+  useEffectUpdate(() => {
+    if (user) {
+      navigate('/')
+    }
+  }, [user])
+
+  const handleLogin = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    login(email, password)
+  }
+
   return (
-    <div>LoginIndex</div>
+    <section className="login-index index">
+      <form className='login' onSubmit={handleLogin}>
+        <input type="email" name="email" id="" placeholder='email' onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="password" id="" placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit'>Login</button>
+      </form>
+      {error && <span>Wrong email or password!</span>}
+    </section>
   )
 }
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,14 @@
 import { addDoc, collection, getDocs, getFirestore, DocumentData } from "firebase/firestore";
 import { storageService } from "./async.storage.service";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase.config";
 
 // Define the User interface
 export interface User {
-    _id: string;
-    phoneNumber: string;
-    isAdmin: boolean;
+    id: string;
+    email: string;
+    password: string
+    // isAdmin: boolean;
 }
 
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
@@ -13,13 +16,13 @@ const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
 export const userService = {
     login,
     logout,
-    signup,
+    // signup,
     getLoggedinUser,
     saveLocalUser,
     getUsers,
     getById,
     remove,
-    update,
+    // update,
 }
 
 declare global {
@@ -35,10 +38,11 @@ async function getUsers(): Promise<User[]> {
         const usersData = await storageService.query('user');
         // Map Firestore document data to User objects
         const users: User[] = usersData.map((userData: DocumentData) => ({
-            _id: userData.id,
-            phoneNumber: userData.phoneNumber,
-            isAdmin: userData.isAdmin
-        }));
+            id: userData.id,
+            email: userData.email,
+            password: userData.password,
+            isAdmin: userData.isAdmin,
+        }))
         return users;
     } catch (error) {
         throw new Error('An error occurred while getting users');
@@ -47,74 +51,67 @@ async function getUsers(): Promise<User[]> {
 
 
 async function getById(userId: string): Promise<User | null> {
-    const user = await storageService.get('user', userId)
-    return user as User; // Assuming the retrieved data is already of type User
+    const user: User = await storageService.get<User>('user', userId)
+    return user
 }
 
 function remove(userId: string): Promise<void> {
     return storageService.remove('user', userId)
 }
 
-async function update({ _id, score }: { _id: string, score: number }) {
-    const user = await storageService.get('user', _id)
-    await storageService.put('user', user)
-    if (getLoggedinUser()?._id === user._id) saveLocalUser(user)
-    return user as User; // Assuming the retrieved data is already of type User
-}
+// async function update({ _id, score }: { _id: string, score: number }) {
+//     const user = await storageService.get('user', _id)
+//     await storageService.put('user', user)
+//     if (getLoggedinUser()?.id === user.id) saveLocalUser(user)
+//     return user as User; // Assuming the retrieved data is already of type User
+// }
 
-async function login(phoneNumber: string): Promise<User | undefined> {
+async function login(email: string, password: string): Promise<void> {
     try {
-        const usersCollection = 'users';
-        const querySnapshot = await getDocs(collection(getFirestore(), usersCollection));
-        let user: User | undefined;
-        querySnapshot.forEach((doc) => {
-            const userData = doc.data();
-            if (userData.phoneNumber === phoneNumber) {
-                user = {
-                    _id: doc.id,
-                    phoneNumber: userData.phoneNumber,
-                    isAdmin: userData.isAdmin
-                };
-                saveLocalUser(user); // Save the user to local storage
-                return;
-            }
-        });
-        if (!user) {
-            throw new Error('User not found');
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+        console.log(userCredential.user)
+        const newUser: User = {
+            id: userCredential.user.uid,
+            email: userCredential.user.email!,
+            password: '',
         }
-        return user;
+
+        saveLocalUser(newUser)
     } catch (error) {
-        console.error('Login failed: ', error);
-        throw new Error('Login failed');
+        console.error(error)
     }
 }
 
-async function signup(phoneNumber: string): Promise<User | undefined> {
-    try {
-        const usersCollection = 'users';
-        const newUser = {
-            phoneNumber,
-            isAdmin: false
-        };
-        const docRef = await addDoc(collection(getFirestore(), usersCollection), newUser);
-        const user: User = {
-            _id: docRef.id,
-            phoneNumber,
-            isAdmin: false
-        };
-        saveLocalUser(user); // Save the user to local storage
-        return user;
-    } catch (error) {
-        console.error('Signup failed: ', error);
-        throw new Error('Signup failed');
-    }
-}
+// async function signup(phoneNumber: string): Promise<User | undefined> {
+//     try {
+//         const usersCollection = 'users';
+//         const newUser = {
+//             email,
+//             password,
+//             isAdmin: false
+//         };
+//         const docRef = await addDoc(collection(getFirestore(), usersCollection), newUser);
+//         const user: User = {
+//             id: docRef.id,
+//             email,
+//             password,
+//             isAdmin: false
+//         };
+//         saveLocalUser(user); // Save the user to local storage
+//         return user;
+//     } catch (error) {
+//         console.error('Signup failed: ', error);
+//         throw new Error('Signup failed');
+//     }
+// }
 
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
 }
 
 function saveLocalUser(user: User): void {
+    console.log('saveLocalUser  user:', user)
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user));
 }
 
