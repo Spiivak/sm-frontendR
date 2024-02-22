13d22737392aff9e32db834263a6720a764bb677
Spiivak <edenspiivak@gmail.com>
2024-02-22 18:46:40 +0200

    Add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { store } from './store/store'
 import { ShiftIndex } from './pages/ShiftIndex'
 import UpdateShift from './pages/UpdateShiftIndex'
-import AppBottomNav from './cmps/AppBottomNav'
 import NowIndex from './pages/NowIndex'
 import GeneralIndex from './pages/GeneralIndex'
 import WorkIndex from './pages/WorkIndex'
+import LoginIndex from './pages/LoginIndex'
 
 // import './App.css'
 
 export function App() {
+
 
   return (
     <>
@@ -18,10 +19,11 @@ export function App() {
         <Router>
           <Routes>
             <Route path="/" element={<ShiftIndex />} />
-            <Route path="/:shiftId/edit" element={<UpdateShift/>}/>
-            <Route path="/now" element={<NowIndex />}/>
-            <Route path="/work" element={<WorkIndex />}/>
-            <Route path="/general" element={<GeneralIndex />}/>
+            <Route path="/:shiftId/edit" element={<UpdateShift />} />
+            <Route path="/now" element={<NowIndex />} />
+            <Route path="/work" element={<WorkIndex />} />
+            <Route path="/general" element={<GeneralIndex />} />
+            <Route path="/login" element={<LoginIndex />} />
           </Routes>
         </Router>
       </Provider>
