600243a221c48c963007b78914f38feade3f76bd
Spiivak <edenspiivak@gmail.com>
2024-02-22 18:47:07 +0200

    Update storage service to work dynamic

--- src/services/async.storage.service.ts
+++ src/services/async.storage.service.ts
@@ -1,16 +1,16 @@
-import { Shift } from "./shift.service"
 
 export interface Entity {
-  _id: string
+  id: string
 }
 
 export interface StorageService {
-  query(entityType: string, delay?: number): Promise<Shift[]>
-  get(entityType: string, entityId: string): Promise<Shift>
-  post(entityType: string, newEntity: Shift): Promise<Shift>
-  put(entityType: string, updatedEntity: Shift): Promise<Shift>
-  remove(entityType: string, entityId: string): Promise<void>
+  query<T extends Entity>(entityType: string, delay?: number): Promise<T[]>;
+  get<T extends Entity>(entityType: string, entityId: string): Promise<T>;
+  post<T extends Entity>(entityType: string, newEntity: T): Promise<T>;
+  put<T extends Entity>(entityType: string, updatedEntity: T): Promise<T>;
+  remove(entityType: string, entityId: string): Promise<void>;
 }
+
 
 export const storageService: StorageService = {
   query,
@@ -20,60 +20,50 @@ export const storageService: StorageService = {
   remove,
 }
 
-function query(entityType: string, delay: number = 500): Promise<Shift[]> {
-  const entities: Shift[] = JSON.parse(localStorage.getItem(entityType) || '[]')
-  return new Promise((resolve) => setTimeout(() => resolve(entities), delay))
+async function query<T extends Entity>(entityType: string, delay: number = 500): Promise<T[]> {
+  const entities: T[] = JSON.parse(localStorage.getItem(entityType) || '[]');
+  return new Promise((resolve) => setTimeout(() => resolve(entities), delay));
 }
 
-function get(entityType: string, entityId: string): Promise<Shift> {
-  return query(entityType).then((entities) => {
-    const entity = entities.find((entity) => entity._id === entityId)
-    if (!entity) {
-      throw new Error(`Get failed, cannot find entity with id: ${entityId} in: ${entityType}`)
-    }
-    return entity
-  })
+async function get<T extends Entity>(entityType: string, entityId: string): Promise<T> {
+  return query<T>(entityType).then((entities) => {
+    const entity = entities.find((entity) => entity.id === entityId);
+    if (!entity) throw new Error(`Get failed, cannot find entity with id: ${entityId} in: ${entityType}`);
+    return entity;
+  });
 }
 
-function post(entityType: string, newEntity: Shift): Promise<Shift> {
-  newEntity = {...newEntity}
-  newEntity._id = _makeId()
-  return query(entityType).then((entities) => {
-    entities.push(newEntity)
-    _save(entityType, entities)
-    return newEntity
-  })
+async function post<T extends Entity>(entityType: string, newEntity: T): Promise<T> {
+  newEntity.id = _makeId();
+  return query<T>(entityType).then((entities) => {
+    entities.push(newEntity);
+    _save(entityType, entities);
+    return newEntity;
+  });
 }
 
-function put(entityType: string, updatedEntity: Shift): Promise<Shift> {
-  updatedEntity = JSON.parse(JSON.stringify(updatedEntity))
-  return query(entityType).then((entities) => {
-    const idx = entities.findIndex((entity) => entity._id === updatedEntity._id)
-    if (idx < 0) {
-      throw new Error(
-        `Update failed, cannot find entity with id: ${updatedEntity._id} in: ${entityType}`
-      )
-    }
-    entities.splice(idx, 1, updatedEntity)
-    _save(entityType, entities)
-    return updatedEntity
-  })
+async function put<T extends Entity>(entityType: string, updatedEntity: T): Promise<T> {
+  return query<T>(entityType).then((entities) => {
+    const idx = entities.findIndex((entity) => entity.id === updatedEntity.id);
+    if (idx < 0) throw new Error(`Update failed, cannot find entity with id: ${updatedEntity.id} in: ${entityType}`);
+    entities[idx] = updatedEntity;
+    _save(entityType, entities);
+    return updatedEntity;
+  });
 }
 
-function remove(entityType: string, entityId: string): Promise<void> {
-  return query(entityType).then((entities) => {
-    const idx = entities.findIndex((entity) => entity._id === entityId)
-    if (idx < 0) {
-      throw new Error(`Remove failed, cannot find entity with id: ${entityId} in: ${entityType}`)
-    }
-    entities.splice(idx, 1)
-    _save(entityType, entities)
-  })
+async function remove(entityType: string, entityId: string): Promise<void> {
+  return query<Entity>(entityType).then((entities) => {
+    const idx = entities.findIndex((entity) => entity.id === entityId);
+    if (idx < 0) throw new Error(`Remove failed, cannot find entity with id: ${entityId} in: ${entityType}`);
+    entities.splice(idx, 1);
+    _save(entityType, entities);
+  });
 }
 
 // Private functions
 
-function _save(entityType: string, entities: Shift[]): void {
+function _save<T extends Entity>(entityType: string, entities: T[]): void {
   localStorage.setItem(entityType, JSON.stringify(entities))
 }
 
